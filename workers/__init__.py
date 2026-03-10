f77b304376aa36ecbed1f0ad6cbccbfead779085
stanlley-locke <stanlleylocke@gmail.com>
2026-03-10 10:03:55 +0300

    celery

--- workers/__init__.py
+++ workers/__init__.py
@@ -4,7 +4,10 @@
 from .encoder import process_upload
 from .auditor import audit_storage_health, repair_object, node_heartbeat
 from .decoder import process_download
-from .garbage_collector import process_garbage_collection
+from .garbage_collector import process_garbage_collection, sweep_deleted_objects
+from .message_delivery import confirm_delivery, expire_old_messages, index_message_for_search, sync_dht_to_db, cleanup_dht_inbox
+from .storage_auditor import audit_nodes
+from .payout_calculator import calculate_payouts
 
 __all__ = [
     'process_upload',
@@ -13,11 +16,12 @@ __all__ = [
     'node_heartbeat',
     'process_download',
     'process_garbage_collection',
+    'sweep_deleted_objects',
     'confirm_delivery',
     'expire_old_messages',
     'index_message_for_search',
     'sync_dht_to_db',
     'cleanup_dht_inbox',
+    'audit_nodes',
+    'calculate_payouts',
 ]
-
-from .message_delivery import confirm_delivery, expire_old_messages, index_message_for_search, sync_dht_to_db, cleanup_dht_inbox
